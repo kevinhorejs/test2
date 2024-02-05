103719b19f1dee15d1f8326ae9bf830cf9df83cf
Kevin <s53149@edu.campus02.at>
2024-02-05 21:15:39 +0100

    2ter

---
+++ src/test/java/BMICalculatorTest.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BMICalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void calculateBMI() {
+    }
+
+    @Test
+    void calculateBMICategory() {
+    }
+
+    @Test
+    void getBMICategoryName() {
+    }
+
+    @Test
+    void setBodyHeight() {
+    }
+
+    @Test
+    void setBodyWeight() {
+    }
+
+    @Test
+    void getFirstname() {
+    }
+
+    @Test
+    void getLastname() {
+    }
+
+    @Test
+    void getBodyHeight() {
+    }
+
+    @Test
+    void getBodyWeight() {
+    }
+
+    @Test
+    void getGender() {
+    }
+}
