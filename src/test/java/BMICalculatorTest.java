dc29c6c09b7db51cbe7140f22754b5877d89a81d
Kevin <s53149@edu.campus02.at>
2024-02-05 21:18:22 +0100

    2ter

--- src/test/java/BMICalculatorTest.java
+++ src/test/java/BMICalculatorTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -5,47 +6,50 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BMICalculatorTest {
 
+    BMICalculator myBMI;
+    BMICalculator yourBMI;
+
+    String name = "Kevin";
+    String nachname = "Horejs";
+    int höhe = 175;
+    double gewicht = 90.0;
+    char gender = 'm';
+
+    String your_name = "Melanie";
+    String your_nachname = "Horejs";
+    int your_höhe = 170;
+    double your_gewicht = 70.0;
+    char your_gender = 'w';
+
     @BeforeEach
-    void setUp() {
+    void createMyBMI(){
+        myBMI = new BMICalculator(
+                name,
+                nachname,
+                höhe,
+                gewicht,
+                gender);
+        yourBMI = new BMICalculator(
+                your_name,
+                your_nachname,
+                your_höhe,
+                your_gewicht,
+                your_gender);
+    }
+    @Test
+    void testKonstruktor(){
+        Assertions.assertEquals(name,myBMI.getFirstname());
+        Assertions.assertEquals(nachname,myBMI.getLastname());
+        Assertions.assertEquals(höhe,myBMI.getBodyHeight());
+        Assertions.assertEquals(gewicht,myBMI.getBodyWeight());
+        Assertions.assertEquals(gender,myBMI.getGender());
+
+        Assertions.assertEquals(your_name,yourBMI.getFirstname());
+        Assertions.assertEquals(your_nachname,yourBMI.getLastname());
+        Assertions.assertEquals(your_höhe,yourBMI.getBodyHeight());
+        Assertions.assertEquals(your_gewicht,yourBMI.getBodyWeight());
+        Assertions.assertEquals(your_gender,yourBMI.getGender());
     }
 
-    @Test
-    void calculateBMI() {
-    }
 
-    @Test
-    void calculateBMICategory() {
-    }
-
-    @Test
-    void getBMICategoryName() {
-    }
-
-    @Test
-    void setBodyHeight() {
-    }
-
-    @Test
-    void setBodyWeight() {
-    }
-
-    @Test
-    void getFirstname() {
-    }
-
-    @Test
-    void getLastname() {
-    }
-
-    @Test
-    void getBodyHeight() {
-    }
-
-    @Test
-    void getBodyWeight() {
-    }
-
-    @Test
-    void getGender() {
-    }
 }
